50bed85d682157611d280bf36238d41a8ecd9627
Robert <52195359+SeparateRecords@users.noreply.github.com>
2021-04-16 09:19:28 +1000

    Remove enumerate's dependency on count and zip

--- src/enumerate.ts
+++ src/enumerate.ts
@@ -1,18 +1,19 @@
-import { count } from "./count.ts";
-import { zip } from "./zip.ts";
-
 /**
  * Yield a tuple of the index and the item.
  * @param iterable The iterable to enumerate.
- * @example
+ *
  * ```
  * [...enumerate("abc")]
  * // => [[1, "a"], [2, "b"], [3, "c"]]
  * ```
  */
-export function enumerate<T>(
+export function* enumerate<T>(
   iterable: Iterable<T>,
   start = 0,
 ): Generator<[number, T]> {
-  return zip(count(start), iterable);
+  let i = start;
+  for (const value of iterable) {
+    yield [i, value]
+    i++;
+  }
 }
